33d4185f27f635f8dbc9399227946e907fb9a2a1
root <root@root-PC>
2019-03-13 06:59:04 -0300

    13/03/2019 examples hibernate

---
+++ src/main/java/model/OneToManyBag/OneToManyBag.java
@@ -0,0 +1,50 @@
+package model.OneToManyBag;
+
+import java.math.BigDecimal;
+
+import dao.AbstractDAO;
+
+public class OneToManyBag {
+
+	public static void main(String [] arg){
+
+		/* 
+		 * PARA ESTE EJEMPLO UN ITEM TIENE MUCHOS BIDS Y MUCHOS BIDS TIENE UN ITEM
+		 * PARA EL CASO DESDE LA CLASE ITEM SE MAPEA ONETOMANY 
+		 * Y DESDE LA CLASE BID SE MAPEA MANYTONE
+		 * ASI MISMO EL ADICIONAR EL MISMO BID MAS DE UNA VEZ A UN ITEM NO LO DUPLICA
+		 * PARA ESTE CASO ES NECESARIO PASAR POR PARAMETRO ITEM A BID
+		 * NO CREO QUE SE CUMPLA LA PERSISTENCIA POR ALCANCE
+		 * */
+		
+		
+		Item someItem = new Item("Primer Item");
+		AbstractDAO.almacenaEntidad(someItem);
+
+		Bid someBid1 = new Bid(new BigDecimal("201.00"), someItem);
+		Bid someBid2 = new Bid(new BigDecimal("202.00"), someItem);
+		Bid someBid3= new Bid(new BigDecimal("203.00"), someItem);
+		Bid someBid4= new Bid(new BigDecimal("203.00"), someItem);
+
+		
+		AbstractDAO.almacenaEntidad(someBid1);
+		AbstractDAO.almacenaEntidad(someBid2);
+		AbstractDAO.almacenaEntidad(someBid3);
+		AbstractDAO.almacenaEntidad(someBid4);
+
+
+		
+		Item item2= new Item("Test de items.... ");
+		AbstractDAO.almacenaEntidad(item2);
+
+		Bid bid2 = new Bid(new BigDecimal("100.00"), item2);
+		AbstractDAO.almacenaEntidad(bid2);
+
+		
+
+	}
+
+
+
+
+}
